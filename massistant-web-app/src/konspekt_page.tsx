c69c327f835bef47a079a465614a89a682a8d164
Nikon Podgorny <podgorny.nikon@yandex.ru>
2023-11-24 00:25:00 +0300

    mimimal for for file upload

--- massistant-web-app/src/konspekt_page.tsx
+++ massistant-web-app/src/konspekt_page.tsx
@@ -1,10 +1,26 @@
-import { AudioFileRounded, UploadFileRounded } from "@mui/icons-material";
+import { AudioFileRounded, UploadFileRounded, Save } from "@mui/icons-material";
 import { Box, Button, FormControl, Input, Stack } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import axios from "axios";
 import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { useQuery } from "react-query";
 
 type konspektInputs = {
     audio: FileList;
+};
+
+const uploadKonspekt = (audio: File) => {
+    const url = import.meta.env.VITE_API_ROOT + "/konspekt";
+
+    let formData = new FormData();
+    formData.append("audio", audio);
+
+    return axios.post(url, formData, {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        }
+    });
 };
 
 export const KonspektPage = () => {
@@ -12,18 +28,36 @@ export const KonspektPage = () => {
         register,
         handleSubmit,
         watch,
-        formState: { errors }
+        formState: { errors },
+        getValues
     } = useForm<konspektInputs>();
-    const onKonspektFormSubmit: SubmitHandler<konspektInputs> = (data) =>
-        console.log(data);
 
-    console.log(watch("audio"))
+    const submitKonspektQuery = useQuery(
+        ["upload-konspekt"],
+        () => {
+            let files = getValues().audio;
+            uploadKonspekt(files[0]);
+        },
+        {
+            enabled: false,
+            onSuccess: (data) => {
+                alert("ok");
+            },
+            onError: (data) => {
+                alert("error");
+            }
+        }
+    );
+
+    const onKonspektFormSubmit: SubmitHandler<konspektInputs> = (_) => {
+        submitKonspektQuery.refetch();
+    };
 
     return (
         <>
             <div>this is a konspekt page</div>
 
-            <form onSubmit={handleSubmit(onKonspektFormSubmit)}>
+            <form onSubmit={() => false}>
                 <Stack spacing={1} sx={{ width: 400 }}>
                     <Button variant="outlined" component="label">
                         <Box sx={{ display: "flex", gap: "0.25rem" }}>
@@ -38,14 +72,25 @@ export const KonspektPage = () => {
                             {...register("audio", { required: true })}
                         />
                     </Button>
-                    <Button
-                        variant="contained"
-                        type="submit"
-                        onClick={handleSubmit(onKonspektFormSubmit)}
-                    >
-                        <UploadFileRounded />
-                        <span>Загрузить</span>
-                    </Button>
+                    {submitKonspektQuery.isFetching ? (
+                        <LoadingButton
+                            loading
+                            loadingPosition="start"
+                            startIcon={<Save />}
+                            variant="outlined"
+                        >
+                            Загрузка...
+                        </LoadingButton>
+                    ) : (
+                        <Button
+                            variant="contained"
+                            type="submit"
+                            onClick={handleSubmit(onKonspektFormSubmit)}
+                        >
+                            <UploadFileRounded />
+                            <span>Загрузить</span>
+                        </Button>
+                    )}
                 </Stack>
             </form>
         </>
