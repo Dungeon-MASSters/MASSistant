b380367c3ee5db7d5b659e1e5c0035421699cece
blazinghorizon <apexkennys@gmail.com>
2023-11-24 00:22:31 +0300

    added getting texts by sentence

--- massistant-ai/prod/sentence_splitter.py
+++ massistant-ai/prod/sentence_splitter.py
@@ -33,17 +33,18 @@ class SyntaxAnalyzer:
         return doc
 
     @staticmethod
-    def concat_sentences(
+    def get_sentences(
         doc: stanza.Document,
         normalize: bool = False,
-        upos: list[str] = []) -> str:
+        upos: list[str] = []) -> list[str]:
         if normalize:
             attribute = 'lemma'
         else:
             attribute = 'text'
 
-        words = []
+        sentences = []
         for sentence in doc.sentences:
+            words = []
             for token in sentence.words:
                 if attribute in dir(token):
                     cur_attr = attribute
@@ -52,9 +53,15 @@ class SyntaxAnalyzer:
 
                 if 'upos' in dir(token) and token.upos in upos or upos == []:
                     words.append(getattr(token, cur_attr))
+
+            sentences.append(' '.join(words))
         
-        return ' '.join(words)
+        return sentences
     
 # NOUN - сущ, VERB или AUX - глагол, ADJ - прилагательное, 
 # ADV - наречие, NUM - числительные, PROPN - местоимения,
 # X - иностранные слова
+
+ppl = SyntaxAnalyzer()
+doc = ppl('Привет. Как дела? Я съел деда.')
+print(ppl.get_sentences(doc, normalize=True, upos=['NOUN', 'VERB', 'AUX']))
