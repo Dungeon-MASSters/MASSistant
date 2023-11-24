557aba8d836e2edc465470cbca28f76080855d5c
genia10 <ge01nia85@gmail.com>
2023-11-24 09:08:46 +0300

    logo.png + login.page

---
+++ massistant-web-app/src/login_page.tsx
@@ -0,0 +1,7 @@
+import {Box, Button, Stack} from "@mui/material";
+import {AudioFileRounded, Save, UploadFileRounded} from "@mui/icons-material";
+import {LoadingButton} from "@mui/lab";
+
+export const HelpPage = () => {
+    return <div>this is a help page</div>;
+};
